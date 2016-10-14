3c501ca1a46f961854bf4d07cd15fda8ef1ad013
Gabriel M <gabriel.majeri6@gmail.com>
2016-10-14 19:21:37 +0300

    Add the File Protocol function pointers

--- Include/EFI/FileIO/FileProtocol.hpp
+++ Include/EFI/FileIO/FileProtocol.hpp
@@ -1,15 +1,35 @@
 #pragma once
 
+#include <cstdint>
+#include <EFI/Status.hpp>
+
 namespace EFI
 {
 	struct FileProtocol
 	{
 		std::uint64_t revision;
-		void* openFile;
-		void* closeFile;
-		void* deleteFile;
+		
+		// These functions have "File" appended to them because "delete" conflicted with the delete operator.
+		
+		Status(*openFile)(FileProtocol*, FileProtocol** newHandle, char16_t* fileName, std::uint64_t openMode, std::uint64_t attributes);
+		void(*closeFile)();
+		void(*deleteFile)();
 		void(*readFile)();
 		void* writeFile;
-
+		
+		void* getPosition;
+		void* setPosition;
+		
+		void* getInfo;
+		void* setInfo;
+		
+		void* flush;
+		
+		// These were added by revision 2:
+		
+		void* openEx;
+		void* readEx;
+		void* writeEx;
+		void* flushEx;
 	};
 }
